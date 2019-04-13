68927d09515b8da1e5028b5353b9ddc965fa69df
Maeve Murphy <maevemurphy530@gmail.com>
2019-04-13 10:03:19 -0700

    Redesign of front page. Moved things to CardView

--- app/src/main/java/com/example/android/fabapp/FrontPageActivity.java
+++ app/src/main/java/com/example/android/fabapp/FrontPageActivity.java
@@ -1,10 +1,15 @@
 package com.example.android.fabapp;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Gallery;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class FrontPageActivity extends AppCompatActivity {
 
@@ -12,6 +17,17 @@ public class FrontPageActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_front_page);
+
+        TextView welcomeText = findViewById(R.id.welcome_text);
+        welcomeText.setText(getString(R.string.text_welcome));
+
+        ImageView projectIcon = findViewById(R.id.projectIcon);
+        int projectResource = getResources().getIdentifier("@drawable/projects_picture", null, this.getPackageName());
+        projectIcon.setImageResource(projectResource);
+
+        ImageView fabricIcon = findViewById(R.id.fabricIcon);
+        int fabricResource = getResources().getIdentifier("@drawable/fabric_picture", null, this.getPackageName());
+        fabricIcon.setImageResource(fabricResource);
 
 
         final Button projectsButton = findViewById(R.id.button_view_projects);
